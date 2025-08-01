b082ceeaeb2b00cc2de9301f75b031684e65cbcf
unknown <zhujiapeng@elegomall.com>
2025-08-01 18:34:21 +0800

    优化地图

--- scenes/map/map_layer.gd
+++ scenes/map/map_layer.gd
@@ -3,10 +3,10 @@ class_name MapLayer
 
 @onready var ground_layer: TileMapLayer = $Ground
 @onready var decoration_layer: TileMapLayer = $Decoration
-@onready var object_layer: TileMapLayer = $Object
-@onready var road_layer: TileMapLayer = $Road
-@onready var building_layer: TileMapLayer = $Building
 @onready var crop_layer: TileMapLayer = $Crop
+@onready var road_layer: TileMapLayer = $Road
+@onready var object_layer: TileMapLayer = $Object
+@onready var building_layer: TileMapLayer = $Building
 
 var dirt_terrain_set: int = 1
 var dirt_terrain_index: int = 0
@@ -20,7 +20,6 @@ func tilling() -> void:
 	var player_position = player.global_position
 	var cell_coords: Vector2i = ground_layer.local_to_map(to_local(player_position))
 	var target_coords: Vector2i = cell_coords + player.direction
-	decoration_layer.erase_cell(target_coords)
 	dirt_cells.append(target_coords)
 	
-	object_layer.set_cells_terrain_connect(dirt_cells, dirt_terrain_set, dirt_terrain_index, false)
+	decoration_layer.set_cells_terrain_connect(dirt_cells, dirt_terrain_set, dirt_terrain_index, false)
